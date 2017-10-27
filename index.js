9bb5a45c382165b3742194737d5e9aae5f2f96d2
Bian Shijun <bian_s@worksap.co.jp>
2017-10-27 15:13:49 +0800

    an early stable version

--- index.js
+++ index.js
@@ -1,51 +1,36 @@
 var _ = require("underscore");
-// Get the modal
-var modal = document.getElementById('myModal');
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
-var mImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-
-img.onclick = function(){
-     modal.style.display = "block";
-     mImg.src = this.src;
-     captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-     modal.style.display = "none";
-}
-
-// analyze the input
+/**
+ * Parses the string and finds the parts of it depending how it is created
+ *
+ * @param {String} string
+ * @returns {Object} object which contains parts href, label, header and buttonHref
+ */
 var getParts = function (string) {
-  var hasHeader, parts, label, href, header;
+  var str, hasHeader, parts, label, href, header;
 
   hasHeader = false;
-  parts = string.split("|");
+  str = string.trim()
+  parts = str.split("|");
 
-  //Get video href
-  imgName = parts[0];
-
+  //Get image prop
+  img = parts[0];
   width = parts[1];
   height = parts[2];
 
   return {
-    imgName     : imgName,
+    img         : img,
     width       : width,
     height      : height,
   };
-
 };
-
 
 module.exports = {
   website: {
     assets: "./book",
+    js: [
+      "script.js"
+    ],
     css: [
       "style.css"
     ]
@@ -55,22 +40,17 @@ module.exports = {
       process: function(blk) {
         var buttonHTML, modalHTML, string, parts, src, label, href;
 
-        parts = getParts(blk.body);
+        string = blk.body;
+        parts = getParts(string);
 
+        buttonHTML = "<a href=\"" + parts.buttonHref + "\" class=\"button play\">" + "zoom in" + "</a>";
+        modalHTML =  "<div class=\"modal modal_video\">"            +
+        "<div class=\"modal_content\"><div class=\"modal_header\">" + "</div><ins class=\"close\"></ins>"  +
+        '<img id=\"myImg\" src=\"' + parts.img + '\" alt=\"image\" width=\"' + parts.width + '\" height=\"' + parts.height + '\">' +
+        "</div>" +
+        "</div>";
 
-         modalHTML =  "<img id=\"myImg\" src=\"" + parts.imgName + "\" alt=\"image\" width=\"" + parts.width + "\" height=\"" + parts.height + "\">" +
-
-          "<div id=\"myModal\" class=\"modal\">" +
-
-            "<span class=\"close\">&times;</span>" +
-
-            "<img class=\"modal-content\" id=\"img01\">" +
-
-            "<div id=\"caption\"></div>"
-          "</div>"
-
-
-        return modalHTML
+        return "<div class=\"modal_parent\">" + buttonHTML + modalHTML + "</div>";
       }
     }
   }
